c74d606168820bea4dbc9dbe3e3e8a9f54d1d8d7
FACUNDO IVAN LEWKOWICZ <48178306@ort>
2025-06-11 09:53:57 -0300

    arrancando

---
+++ Celulares/src/componentes/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer" id="contacto">
+      <p>© 2025 Celulares. Marca registrada por Lewko y Milo's coorporation</p>
+    </footer>
+  );
+}
+
+export default Footer;
